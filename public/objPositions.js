de2ba1940d77e907b047a325306994af12e76e76
AndreaDonati <andrewgifted@users.noreply.github.com>
2020-08-13 11:33:10 +0200

    Trying rotations of bumper and start loading big ball

--- public/objPositions.js
+++ public/objPositions.js
@@ -30,11 +30,19 @@ function getPullerLocalMatrix(run) {
     return utils.MakeWorld(-2.5264, 8.3925, -7.1 - run, 0, -90, 0, 1);
 }
 
+/* 
+    MakeWorld: function (tx, ty, tz, rx, ry, rz, s)
+
+    t: traslazione
+    r: rotazione
+    s: scaling
+*/
+
 var ballLocalMatrix         = utils.MakeWorld( 0,         0,        0,           0,        0,       0,     1); // #0
 var bodyLocalMatrix         = utils.MakeWorld( 0,         0,        0,           0,        0,       0,     1);
-var bumper1LocalMatrix      = utils.MakeWorld( 1.1819,    9.1362,   0.020626,   -6.51,     0,       0,     1);
-var bumper2LocalMatrix      = utils.MakeWorld(-1.5055,    9.1362,   0.020626,   -6.51,     0,       0,     1);
-var bumper3LocalMatrix      = utils.MakeWorld(-0.11626,   9.1362,   -1.020626,   -6.51,     0,       0,     1);
+var bumper1LocalMatrix      = utils.MakeWorld( 1.1819,    9.1362,   0.020626,   -6.51,     -6.51,       -6.51,     1);
+var bumper2LocalMatrix      = utils.MakeWorld(-1.5055,    9.1362,   0.020626,   -6.51,     -6.51,       -6.51,     1);
+var bumper3LocalMatrix      = utils.MakeWorld(-0.11626,   9.1362,   -1.020626,   -6.51,     -6.51,       -6.51,     1);
 var dl1LocalMatrix          = utils.MakeWorld( 0.4366,   12.789,    4.1852,      0,     -101,       0,     1);
 var dl2LocalMatrix          = utils.MakeWorld( 0.713,    12.789,    4.1852,      0,     -101,       0,     1);
 var dl3LocalMatrix          = utils.MakeWorld( 0.9923,   12.789,    4.1852,      0,     -101,       0,     1);
@@ -47,15 +55,17 @@ var dr3LocalMatrix          = utils.MakeWorld(-2.2716,   12.789,    4.1852,     
 var dr4LocalMatrix          = utils.MakeWorld(-1.8722,   12.789,    4.1852,      0,     -101,       0,     1);
 var dr5LocalMatrix          = utils.MakeWorld(-1.5958,   12.789,    4.1852,      0,     -101,       0,     1);
 var dr6LocalMatrix          = utils.MakeWorld(-1.316,    12.789,    4.1852,      0,     -101,       0,     1);
-var leftButtonLocalMatrix   = utils.MakeWorld( 2.6175,    8.7853,  -6.6902,      0,        0,     -90,     1); // #17
-var leftFlipperLocalMatrix  = utils.MakeWorld( 0.6906,    8.4032,  -5.6357,     29.8,     -3.24,   -5.64,  1); // #18
-var pullerLocalMatrix       = utils.MakeWorld(-2.5264,    8.3925,  -7.5892,      0,      -90,       0,     1); // #19
-var rightButtonLocalMatrix  = utils.MakeWorld(-2.97,      8.7853,  -6.6902,      0,        0,      90,     1); // #20
-var rightFlipperLocalMatrix = utils.MakeWorld(-1.307,     8.4032,  -5.6357,    150,       -3.24,   -5.64,  1); // #21
-var leftSlingshotMatrix = utils.MakeWorld(1.1,    8.5,   -4.50626,   180,     0,       0,     1);
-var rightSlingshotMatrix = utils.MakeWorld(-1.6,    8.5,   -4.50626,   90,     0,       0,     1);
-var leftObstacleMatrix = utils.MakeWorld(1.1,    9,   -2.5,   180,     0,       0,     1);
-var rightObstacleMatrix = utils.MakeWorld(-1.6,    9,   -2.5,   180,     0,       0,     1);
+var leftButtonLocalMatrix   = utils.MakeWorld( 2.6175,    8.7853,  -6.6902,      0,        0,     -90,     1);  // #17
+var leftFlipperLocalMatrix  = utils.MakeWorld( 0.6906,    8.4032,  -5.6357,     29.8,     -3.24,   -5.64,  1);  // #18
+var pullerLocalMatrix       = utils.MakeWorld(-2.5264,    8.3925,  -7.5892,      0,      -90,       0,     1);  // #19
+var rightButtonLocalMatrix  = utils.MakeWorld(-2.97,      8.7853,  -6.6902,      0,        0,      90,     1);  // #20
+var rightFlipperLocalMatrix = utils.MakeWorld(-1.307,     8.4032,  -5.6357,    150,       -3.24,   -5.64,  1);  // #21
+var leftSlingshotMatrix     = utils.MakeWorld(1.1,    8.5,   -4.50626,   180,     0,       0,     1);           //
+var rightSlingshotMatrix    = utils.MakeWorld(-1.6,    8.5,   -4.50626,   90,     0,       0,     1);           //
+var leftObstacleMatrix      = utils.MakeWorld(1.1,    9,   -2.5,   180,     0,       0,     1);                 //
+var rightObstacleMatrix     = utils.MakeWorld(-1.6,    9,   -2.5,   180,     0,       0,     1);                //
+var bigBallLocalMatrix      = utils.MakeWorld( 0,         0,        0,           0,        0,       0,     1);  // 
+
 
 var allLocalMatrices = [ballLocalMatrix, bodyLocalMatrix, bumper1LocalMatrix, bumper2LocalMatrix, bumper3LocalMatrix, dl1LocalMatrix, dl2LocalMatrix, dl3LocalMatrix,
                         dl4LocalMatrix, dl5LocalMatrix, dl6LocalMatrix, dr1LocalMatrix, dr2LocalMatrix, dr3LocalMatrix, dr4LocalMatrix, dr5LocalMatrix, dr6LocalMatrix,

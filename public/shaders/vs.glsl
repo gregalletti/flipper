2d20ec51910dcf737048ddb59fe2fc48781dc919
AndreaDonati <andrewgifted@users.noreply.github.com>
2020-08-17 11:34:10 +0200

    actually fixing vs syntax

--- public/shaders/vs.glsl
+++ public/shaders/vs.glsl
@@ -2,7 +2,11 @@
 
 in vec3 inPosition;
 in vec3 inNormal;
+in vec2 in_uv;
+
+out vec2 fsUV;
 out vec3 fsNormal;
+out vec3 fs_pos;
 
 uniform mat4 matrix; 
 uniform mat4 nMatrix;     //matrix to transform normals
